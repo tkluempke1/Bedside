2482788a344520d8c6942b16ca3efee15527354d
tkluempke1 <tkluempke@gmail.com>
2025-09-16 21:19:41 -0700

    Create apps/web/next.config.js

---
+++ apps/web/next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
+  compiler: {
+    // Enable styled components or other compiler options here if necessary
+  }
+};
+
+module.exports = nextConfig;
